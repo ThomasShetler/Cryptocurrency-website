a6da9a8fa7c8c5faa906bf80faa706aaa97829ef
Thomas Shetler <41353290+ThomasShetler@users.noreply.github.com>
2020-02-06 12:15:10 -0800

    Updated commented code

--- mainapp/CryptoCurrency/views.py
+++ mainapp/CryptoCurrency/views.py
@@ -14,7 +14,7 @@ response = requests.get(query)
 
 print(response.json())
 
-def get_bit(request, name):
+def get_bit(request, name): #api pull
     print("@@@@",name) ##just a test
     query = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={}&to_currency={}&apikey={}".format('USD', name, key)
     form = InfoForm(request.POST or None)
@@ -22,9 +22,9 @@ def get_bit(request, name):
     #print('!!!!', response.json()['Realtime Currency Exchange Rate']['6. Last Refreshed'])
     context = {'response': response.json()['Realtime Currency Exchange Rate'],
                'form': form}
-    print('!!!!', context['response']['6. Last Refreshed'])
-    print(type(context['response']))
-    return render(request, 'CryptoCurrency/crypto_api.html', context)
+    print('!!!!', context['response']['6. Last Refreshed']) #test
+    print(type(context['response'])) #test
+    return render(request, 'CryptoCurrency/crypto_api.html', context) #returns the render to the API page
 
 def home(request):
     return render(request, 'CryptoCurrency/crypto_home.html')
@@ -32,7 +32,7 @@ def home(request):
 def details(request, pk):
 
     pk = int(pk)  # Casts value of pk to an int so it's in the proper form
-    info = get_object_or_404(Info, pk=pk)  # Gets single instance of the jersey from the database
+    info = get_object_or_404(Info, pk=pk)  # Gets single instance of the info from the database
     context = {'info': info}  # Creates dictionary object to pass the info object
     return render(request, 'CryptoCurrency/crypto_details.html', context)
 # Create your views here.
@@ -40,16 +40,16 @@ def details(request, pk):
 def add_collection(request):
     form = InfoForm(request.POST or None)     #Gets the posted form, if one exists
     if form.is_valid():                         #Checks the form for errors, to make sure it's filled in
-        form.save()                             #Saves the valid form/jersey to the database
-        return redirect('cryptoList')                #Redirects to the index page, which is named 'footy' in the urls
+        form.save()                             #Saves the valid form/info to the database
+        return redirect('cryptoList')                #Redirects to the index page, which is named 'cryptoList' in the urls
     else:
         print(form.errors)                      #Prints any errors for the posted form to the terminal
         form = InfoForm()                     #Creates a new blank form
     return render(request, 'CryptoCurrency/crypto_create.html', {'form':form})
 
 def index(request):
-    get_Info = Info.info.all()      #Gets all the current jerseys from the database
-    context = {'Info': get_Info}      #Creates a dictionary object of all the jerseys for the template
+    get_Info = Info.info.all()      #Gets all the current info from the database
+    context = {'Info': get_Info}      #Creates a dictionary object of all the info for the template
     return render(request, 'CryptoCurrency/crypto_index.html', context)
 
 def edit(request, pk):
